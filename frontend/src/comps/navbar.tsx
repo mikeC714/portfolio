4d0917b57097b803d882c36daa49037dff351efa
Mike Carter <carter71404@icloud.com>
2026-09-19 03:36:30 -0400

    refactoring the styling

--- frontend/src/comps/navbar.tsx
+++ frontend/src/comps/navbar.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faLinkedin, faDiscord } from "@fortawesome/free-brands-svg-icons";
 type PROPS = {
-	toggleEvent:() => void
+	toggleEvent:() => void;
 };
 
 export function NavBar({ toggleEvent }:PROPS){
 	return(
-		<nav>
-			<ul>
-				<div className="mainNavContainer">
+		<nav className="mainNav">
+		<div className="mainNavListContainer">
+				<ul className="mainNavList">
 					<Link to="/"><li>Home</li></Link>
 					<Link to="/about"><li>About</li></Link>
 					<Link to="/projects"><li>View Projects</li></Link>
@@ -20,8 +21,22 @@ export function NavBar({ toggleEvent }:PROPS){
 						Vote 	
 					</button>
 					</li>
-				</div>
-			</ul>
+				</ul>
+			</div>
 		</nav>	
 	)
 }
+
+				// <div className="contactBar">	
+				// 	<ul>
+				// 		<a href=""className="contactContainer" >
+				// 			<li className="githubIcon"><FontAwesomeIcon icon={faGithub}/></li>
+				// 		</a>
+				// 		<a href="" className="contactContainer">
+				// 			<li className="linkedinIcon"><FontAwesomeIcon icon={faLinkedin}/></li>
+				// 		</a>
+				// 		<a href="" className="contactContainer">
+				// 			<li className="discordIcon"><FontAwesomeIcon icon={faDiscord}/></li>
+				// 		</a>
+				// 	</ul>
+				// </div>
--- frontend/src/pages/mainpage.tsx
+++ frontend/src/pages/mainpage.tsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NavBar } from "../comps/navbar.tsx";
-import { ContactBar } from "../comps/contactbar.tsx";
 import { EventDropDown } from "../comps/event.tsx";
 import { Link } from "react-router-dom";
 import { useVotes } from "../vote.hooks.tsx";
@@ -13,6 +12,15 @@ export function MainPage(){
 	const { mutate, updateSuccess, updateLoading, updateIsErr, updateErr } = update();
 	const [isVisible, setIsVisible] = useState<boolean>(false);
 	const [selectedVote, setSelectedVote] = useState<null | string>(null);
+	const [showPointer, setShowPointer] = useState<boolean>(false);
+	
+	// run once
+	// set pointer to be visible 
+	// triggering a pointer to appear pointing towards the voting to encourage people to vote
+	useEffect(() => {
+		setShowPointer(true);	
+		setTimeout(() => setShowPointer(false), 4000)
+	},[]);
 	
 	const handleVote = (lang:string) => {
 		setSelectedVote(lang);	
@@ -39,14 +47,27 @@ export function MainPage(){
 					</div>
 				)
 			}
-			<header>
+			<header className="header">
 				<NavBar
 					toggleEvent={handleOpenVote}
 				/>
 			</header>
-			<div className="contactBarContainer">
-				<ContactBar /> 
-			</div>
+				{
+					showPointer &&(
+						<div className="pointerContainer">
+							<div className="pointerContent">
+								<div className="arrowContainer">
+									<div className="arrowShaft">
+										<div className="arrowHead"></div>
+									</div>
+								</div>
+							</div>
+							<div className="pointerTxtContainer">
+								<p className="pointerTxT">Help me decide what language I should learn next :)</p>
+							</div>
+						</div>
+					)
+				}
 			<div className="mainPageBody">	
 				<div className="mainPageSubTxt1">
 					<header className="mainSubTxtHeader">
